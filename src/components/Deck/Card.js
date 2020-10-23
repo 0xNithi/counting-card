4c35720ea027d9822402b28cf1ed0ee3f3417df0
MrNithi <bossrocker777@gmail.com>
2020-10-23 21:44:31 +0700

    :lipstick: Add handler to Card

--- src/components/Deck/Card.js
+++ src/components/Deck/Card.js
@@ -1,8 +1,22 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 const card = (props) => {
 	const [select, setSelect] = useState(false)
-	return <img className='w-1/6' src={props.image} alt={props.image} />
+	const handler = () => {
+		setSelect(!select)
+	}
+
+	return (
+		<img
+			className='w-1/13'
+			src={props.image}
+			alt={props.image}
+			onClick={handler}
+			style={{
+				filter: `${select ? 'invert(100%)' : 'none'}`,
+			}}
+		/>
+	)
 }
 
 export default card
